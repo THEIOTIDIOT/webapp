c5016e720c8d48a992954b7b170a16facbbe2ef9
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-26 03:44:31 +0000

    deployment tweaks

--- benzimmer-blog/run_container.sh
+++ benzimmer-blog/run_container.sh
@@ -3,4 +3,5 @@ sudo docker run --rm --name webapp \
     --network zimnet \
     -d -p 8080:80 \
     benzimmer-us:latest \
-    && sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs
+    && sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs \
+    && sudo docker restart swag
