0e4663d8f4fbc7007d0ba56d9805a8b312f66c14
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-26 03:36:06 +0000

    config change

--- benzimmer-blog/run_container.sh
+++ benzimmer-blog/run_container.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo docker run -p 8080:80 --rm --name webapp benzimmer-us:latest \
+sudo docker run --rm --name webapp \
+    --network zimnet \
+    -d -p 8080:80 \
+    benzimmer-us:latest \
     && sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs
