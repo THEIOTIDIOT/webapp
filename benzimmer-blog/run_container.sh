39795329518b0c983e6bbed3bcc210811f9abbd2
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-26 03:11:14 +0000

    naming

--- benzimmer-blog/run_container.sh
+++ benzimmer-blog/run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -it -p 8080:80 --rm --name blog blog-benzimmer-us:latest \
-    && sudo docker cp $HOME/Git/webapp/benzimmer-blog/blog.subdomain.conf swag:/config/nginx/proxy-confs
+sudo docker run -it -p 8080:80 --rm --name webapp benzimmer-us:latest \
+    && sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs
