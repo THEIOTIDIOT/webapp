a7d626d0f157fef7dbe4816206eece3c784084ca
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-26 03:29:43 +0000

    remove tty

--- benzimmer-blog/run_container.sh
+++ benzimmer-blog/run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -it -p 8080:80 --rm --name webapp benzimmer-us:latest \
+sudo docker run -p 8080:80 --rm --name webapp benzimmer-us:latest \
     && sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs
