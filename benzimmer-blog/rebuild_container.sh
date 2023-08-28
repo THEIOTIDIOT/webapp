b13784af41deeb79ebfa902569b7c8f9006e7d11
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-28 03:47:22 +0000

    removed pipe

--- benzimmer-blog/rebuild_container.sh
+++ benzimmer-blog/rebuild_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker stop webapp \
-| sudo docker build -t benzimmer-us:latest . \
-| bash run_container.sh
+&& sudo docker build -t benzimmer-us:latest . \
+&& bash run_container.sh
