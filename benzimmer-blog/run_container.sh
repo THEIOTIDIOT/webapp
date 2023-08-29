3edb5d022f607d59671e652f29acb4b7f44bfcc2
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-29 03:33:00 +0000

    restart unless stopped

--- benzimmer-blog/run_container.sh
+++ benzimmer-blog/run_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo docker run --rm --name webapp \
+sudo docker run --name webapp \
     --network zimnet \
     -d -p 8080:80 \
-    benzimmer-us:latest \
+    --restart=unless-stopped
+    benzimmer-us:latest
 
 sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs
 
