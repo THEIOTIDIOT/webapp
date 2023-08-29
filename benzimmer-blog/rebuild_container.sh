fa5b3ab39587115fac1405215fd40f85c1204b80
THEIOTIDIOT <benzimmer.is@gmail.com>
2023-08-29 03:25:54 +0000

    added subfolder proxy config

--- benzimmer-blog/rebuild_container.sh
+++ benzimmer-blog/rebuild_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo docker stop webapp \
-&& sudo docker build -t benzimmer-us:latest . \
-&& bash run_container.sh
+sudo docker stop webapp
+
+sudo docker build -t benzimmer-us:latest .
+
+bash run_container.sh
--- benzimmer-blog/run_container.sh
+++ benzimmer-blog/run_container.sh
@@ -3,5 +3,9 @@ sudo docker run --rm --name webapp \
     --network zimnet \
     -d -p 8080:80 \
     benzimmer-us:latest \
-    && sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs \
-    && sudo docker restart swag
+
+sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subdomain.conf swag:/config/nginx/proxy-confs
+
+sudo docker cp $HOME/Git/webapp/benzimmer-blog/webapp.subfolder.conf swag:/config/nginx/proxy-confs
+
+sudo docker restart swag
